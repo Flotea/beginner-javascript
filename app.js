67bc168535f26c3172177437e446aa65fd930fd7
Flotea <hgrimmer0786@myhta.org>
2017-03-10 14:29:36 -1000

    ok

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 function fortuneTeller(){
+	var numOfKids = 3;//number
+	var partnerName = "Mr.NiceGuy";//string
+	var geographicLocation = "home";//string
+	var jobTitle = "animator";//string
+	alert("You will be a " + jobTitle + " at " + geographicLocation + ", and married to " + partnerName + " with " + numOfKids + " kids." )
 
 }
+
+fortuneTeller();
 
 function ageCalculator(){
+	var birthYear = 2002;//number
+	var futureYear = 2030;//number
+	var howOldWillIBe = 2030 - 2002;//number
+	alert("You will be either " + howOldWillIBe + " or 27 in " + futureYear + "depending on the month");
+
 
 }
+
+ageCalculator();
